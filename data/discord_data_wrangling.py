3cb43b0772e6a2757522113a8c26eaae53b36bc5
ztoms <tommyzhao111@gmail.com>
2020-11-13 18:16:33 -0500

    Wrangling extracted json data from Discord

---
+++ data/discord_data_wrangling.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 07:24:14 2020
+
+@author: tommy zhao
+
+Basic data cleaning for Discord Data
+"""
+
+import json
+import re
+from collections import defaultdict
+
+files = ['flat-earth-discussions.json', 'saturday-nights-with-the-boys.json', 
+         'intellectual-thought.json']
+
+messages = []
+for file in files:
+    with open(file) as f:
+         data = json.load(f)
+        
+    # get message log
+    messages = messages + data['messages']
+
+# get set of users
+users = set([(msg['author']['id'], msg['author']['name']) for msg in messages])
+
+# clean text and convert it to training format
+relevant_users = set(['Wanderer', 'Pandoge', 'blownking', 'crmbob5', 'Fl3tchr', 'mrbabybob', 'BenjiMcmuscles', 'ztoms', 'mynameismili', 'Dingo'])
+
+prev_author = '' #keep track of prev author
+prev_content = '' #track previous content to avoid repeats
+text = ''
+for msg in messages:
+    author = msg['author']['name']
+    content = msg['content']
+    #skip duplicate messages from the same author
+    if content == prev_content and author == prev_author:
+        continue
+    prev_content = content
+    #remove links
+    content = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', content)
+    #strip whitespace
+    content = content.strip()
+    #skip empty messages
+    if not content: continue
+    #skip messages that are only contain mentions
+    if re.sub("(@[A-Za-z0-9_]+)","", content).strip() == "": continue
+    #skip messages that only contain numbers and/or periods
+    if not re.compile(r'[^0-9.]').search(content): continue
+    #skip messages that end in .net or .com
+    if content.endswith('.net') or content.endswith('.com'): continue
+    #get rid of bot commands
+    if content.startswith('/') or content.startswith('-'): continue
+    #skip 'Joined the server' and 'Pinned a message' messages
+    if content == 'Joined the server.' or content == 'Pinned a message.' :
+        continue
+    # if the author is the same as before, don't add an <|end|> token
+    if author == prev_author:
+        text = text + msg['content'] + ' \n '
+    else:
+        prev_author = author
+        # remove users the programmer doesn't care about
+        if author not in relevant_users:
+            author = 'rando'
+        text = text + f"<|end|> \n {author}: \n {content} \n "
+    
+with open('output_data.txt', 'w', encoding = 'utf-8') as f:
+    f.write(text)
