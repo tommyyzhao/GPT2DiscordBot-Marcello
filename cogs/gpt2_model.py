e8477f491c9744c22835ae766296253b967bafa3
ztoms <tommyzhao111@gmail.com>
2020-11-14 09:33:29 -0500

    Using Discort chat history to generate a historical prompt
    -BoboBot added for testing

--- cogs/gpt2_model.py
+++ cogs/gpt2_model.py
@@ -7,7 +7,7 @@ import time
 import re
 import os
 
-checkpoint_model = 'run_flat_earth_v6'
+checkpoint_model = 'run_flat_earth_v7'
 # some fun rejoin messages
 ON_CONNECT_SENTENCES = ['I am back.',
                      'I have returned.',
@@ -49,6 +49,8 @@ class TextGen(commands.Cog):
         
         self.client = client
         self.init_model()
+        self.monologue = False
+        self.fake_history = True
         
     def init_model(self):
         self.sess = gpt2.start_tf_sess()
@@ -60,8 +62,8 @@ class TextGen(commands.Cog):
         print('Bot is online with TextGen.')
         self.simulator_channel = self.client.get_channel(int(776592860723937301))
         if self.simulator_channel is not None:
-            await self.simulator_channel.send("Flat-Earth Model V6.0 loaded")
-            #await self.simulator_channel.send(ON_CONNECT_SENTENCES[random.randrange(len(ON_CONNECT_SENTENCES))])
+            #await self.simulator_channel.send("BoboBot has been loaded")
+            await self.simulator_channel.send(ON_CONNECT_SENTENCES[random.randrange(len(ON_CONNECT_SENTENCES))])
         
     @commands.Cog.listener()
     async def on_connect(self):
@@ -73,7 +75,61 @@ class TextGen(commands.Cog):
         
     @commands.Cog.listener()
     async def on_message(self, message):
-        print(f'Message sent from channel {message.channel} from author {message.author} with content: {message.content}')
+        print(f'ON_MESSAGE: Message sent from channel {message.channel} from author {message.author} with content: {message.content}')
+        history_prompt = ''
+        if self.monologue:
+            start = time.time()
+            content = []
+            if not self.fake_history:
+                async for msg in message.channel.history(limit=10):
+                    content.append(msg.content)
+                content.reverse() #reverse last 10 messages to build the prompt chronologically
+                
+                for text in content:
+                    history_prompt = history_prompt + f"<|end|> \n {text} \n "
+                history_prompt = history_prompt[8:]
+            else:
+                with open('./data/output/fakehistory.txt') as f:
+                    history_prompt = f.read()
+            
+            logging.info(f'TIME: Generating historical prompt took {round(time.time() - start, 2)} seconds')
+            #augment prompt with contextual information for GPT-2
+            users = ['Wanderer', 'Pandoge', 'blownking', 'crmbob5', 'Fl3tchr', 'mrbabybob', 'BenjiMcmuscles', 'ztoms', 'mynameismili', 'rando']
+            begin_token = '\n b<|end|>'
+            augmented_prompt = f"{history_prompt} {begin_token} \n {users[random.randrange(len(users))]}:"
+            print(augmented_prompt)
+            
+            response = gpt2.generate(self.sess,
+                run_name=checkpoint_model,
+                length=100,
+                temperature=0.75,
+                top_p=0.9,
+                prefix=augmented_prompt,
+                nsamples=4,
+                return_as_list=True
+                )[random.randrange(4)]
+            
+            logging.info(f'TIME: Generating monologue response took {round(time.time() - start, 2)} seconds')
+            print('response PRE PROCESS: ' + response)
+            # remove start token
+            response = response[response.index(begin_token)+len(begin_token):]
+            # check for end token
+            if response[:response.index('<|end|>')] == -1:
+                print('no end token generated')
+                return
+            
+            while not response[:response.index('<|end|>')]:
+                print("CHANGING: " + response)
+                response = response[response.index('<|end|>')+7:]
+            response = response[:response.index('<|end|>')]
+            
+            print('MONOLOGUE RESPONSE: ' + response)
+            
+            # select random text from generated texts
+            logging.info(f'RESPONSE: monologue response:\n{response}')
+            
+            await self.simulator_channel.send(response)
+                
         
     @commands.command()
     @commands.is_owner()
@@ -81,6 +137,18 @@ class TextGen(commands.Cog):
         if self.simulator_channel is not None:
             await self.simulator_channel.send(ON_SHUTDOWN_SENTENCES[random.randrange(len(ON_SHUTDOWN_SENTENCES))])
         await ctx.bot.logout()
+        
+    @commands.command()
+    async def toggle_fake_history(self, ctx):
+        self.fake_history = not self.fake_history
+        
+    @commands.command()
+    async def start_simulation(self, ctx):
+        self.monologue = True
+        
+    @commands.command()
+    async def stop_simulation(self, ctx):
+        self.monologue = False
         
     @commands.command()
     async def poke(self, ctx):
@@ -243,6 +311,51 @@ class TextGen(commands.Cog):
             # select random text from generated texts
             response = filtered_responses[random.randrange(len(filtered_responses))]
             logging.info(f'\BenjiBot chosen response:\n{response}')
+            
+            await ctx.send(response)
+            
+    @commands.command()
+    async def BoboBot(self, ctx, *, prompt):
+        async with ctx.typing():
+            start = time.time()
+            print(f'BoboBot received msg: {prompt} from channel {ctx.message.channel} with msg: {ctx.message.content}')
+            logging.info(f'BoboBot received msg: {prompt} from channel {ctx.message.channel}')
+            
+            #augment prompt with contextual information for GPT-2
+            augmented_prompt = f" {str(ctx.message.author)[:5]}: \n {prompt} \n blownking:"
+            logging.info(f'Augmented prompt to be:\n{augmented_prompt}')
+            
+            texts = gpt2.generate(self.sess,
+                run_name=checkpoint_model,
+                length=100,
+                temperature=0.75,
+                top_p=0.9,
+                prefix=augmented_prompt,
+                truncate="<|end|>",
+                return_as_list=True,
+                nsamples=6
+                )
+            logging.info(f'Generating response took {round(time.time() - start, 2)} seconds')
+            
+            filtered_responses = []
+            for i, text in enumerate(texts):
+                # clean generated text
+                clean_text = ''.join(text.split("blownking:")[1:]) #in case no end token generated, split on username
+                clean_text = re.sub(' +', ' ', clean_text)
+                clean_text = clean_text.strip()
+                logging.info(f'BoboBot cleaned response {i}:\n{clean_text}')
+                #check for low effort responses
+                if len(clean_text.split()) <= 1:
+                    # discourage one word responses
+                    logging.info(f'response {i} is less than 2 characters long')
+                    if random.random() > 0.5:
+                        print('filtering short response')
+                        continue
+                filtered_responses.append(clean_text)
+            
+            # select random text from generated texts
+            response = filtered_responses[random.randrange(len(filtered_responses))]
+            logging.info(f'BoboBot chosen response:\n{response}')
             
             await ctx.send(response)
                 
