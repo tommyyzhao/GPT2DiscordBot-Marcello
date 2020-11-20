57bcfa5f0fbf50b5e809a511ba34a8a818f1aebf
ztoms <tommyzhao111@gmail.com>
2020-11-20 17:07:54 -0500

    Some test functions in friend_bot.py, added checkpoint folder

--- friend_bot.py
+++ friend_bot.py
@@ -6,12 +6,22 @@ client = commands.Bot(command_prefix = '.')
 
 #loading and unloading the GPT2 model actually doesn't work, don't use
 @client.command()
+@commands.is_owner()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
     
 @client.command()
+@commands.is_owner()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
+    
+@client.command()
+async def quoteTest(ctx, *, message):
+    await ctx.send(f"> {message}")
+    
+@client.command()
+async def emojiTest(ctx, *, message):
+    await ctx.send(f":{message}:")
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
